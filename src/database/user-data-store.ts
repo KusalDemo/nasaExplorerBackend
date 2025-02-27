33c2bbd3b84fd39c249be381229233aeaf1f4e32
KusalDemo <kusalgunasekara2002@gmail.com>
2025-02-27 22:51:29 +0530

    Implemented user data store and user routes

---
+++ src/database/user-data-store.ts
@@ -0,0 +1,99 @@
+import IUser from "../models/IUser";
+import {User} from "../models/User";
+
+export const registerUser = async (user: User) => {
+
+    try {
+        const fetchedUser = await IUser.findOne({email: user.email});
+
+        if (!fetchedUser) {
+            /*const encryptedPassword = await bcrypt.hash(user.password, 10);*/
+            const newUser = new User(user.name, user.email, user.password, user.articles, user.comments);
+
+            await IUser.create(newUser);
+            return newUser;
+        } else {
+            throw new Error(`User already exists`);
+        }
+    } catch (error) {
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const loginUser = async (user: User) => {
+    try {
+        const fetchedUser = await IUser.findOne({email: user.email});
+
+        if (fetchedUser) {
+            /* const isPasswordValid = await bcrypt.compare(user.password, fetchedUser.password);*/
+            const passwordMatch = fetchedUser.password.trim() === user.password.trim();
+            if (!passwordMatch) {
+                throw new Error("Invalid email or password");
+            }
+            console.log(`User logged in successfully : ${fetchedUser}`);
+            return fetchedUser;
+        }
+        return null;
+    } catch (error) {
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const updateUser = async (id:string, user: User) => {
+    try{
+        await IUser.updateOne({_id:id}, user);
+        return user;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const addUserArticle = async (id:string, articleId: string | any ) => {
+    try{
+        const updatedUser = await IUser.findByIdAndUpdate(id,
+            {
+                $push: {articles: articleId},
+            },
+            {new: true}
+        );
+        if (!updatedUser) {
+            throw new Error(`User with id : ${id} not found`);
+        }
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const addUserComment = async (id:string, commentId: string | any ) => {
+    try{
+        const updatedUser = await IUser.findByIdAndUpdate(id,
+            {
+                $push: {comments: commentId},
+            },
+            {new: true}
+        );
+        if (!updatedUser) {
+            throw new Error(`User with id : ${id} not found`);
+        }
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const deleteUserComment = async (id:string, commentId: string | any ) => {
+    try{
+        const updatedUser = await IUser.findByIdAndUpdate(id,
+            {
+                $pull: {comments: commentId},
+            },
+            {new: true}
+        );
+        if (!updatedUser) {
+            throw new Error(`User with id : ${id} not found`);
+        }
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+
---
+++ src/routes/user-routes.ts
@@ -0,0 +1,75 @@
+import express from 'express';
+import {loginUser, registerUser, updateUser} from "../database/user-data-store";
+import {User} from "../models/User";
+import {generateToken, verifyToken} from "../util/jwt";
+import dotenv from "dotenv";
+
+dotenv.config();
+let userRouter = express.Router();
+
+userRouter.post("/register", async (req, res) => {
+    try{
+        const user: User = req.body;
+        let response = await registerUser(user);
+        res.send(`User logged in successfully : ${response}`);
+    }catch (error){
+        console.error(`Error occurred: ${error}`);
+        error instanceof Error ? res.status(400).send(error.message) : res.status(500).send(error);
+    }
+})
+
+userRouter.post("/login",async (req, res) => {
+    try{
+        const user = req.body;
+        let response = await loginUser(user);
+        if(response){
+            const { accessToken, refreshToken } =await generateToken(response.email);
+            res.status(200).json({ user: response, accessToken: accessToken, refreshToken: refreshToken });
+        }else{
+            res.status(401).send("Invalid email or password");
+        }
+    }catch (error){
+        console.error(`Error occurred: ${error}`);
+        error instanceof Error ? res.status(400).send(error.message) : res.status(500).send(error);
+    }
+})
+
+userRouter.put("/update/:id", async (req, res) => {
+    try{
+        const userEmail = req.params.id;
+        let user:User = req.body;
+        let updatedUser = await updateUser(userEmail,user);
+        res.status(204).send(updatedUser);
+    }catch (error){
+        console.error(`Error occurred: ${error}`);
+        error instanceof Error ? res.status(400).send(error.message) : res.status(500).send(error);
+    }
+})
+
+
+export async function authenticateToken(req : express.Request, res : express.Response, next : express.NextFunction){
+    if (req.method === 'GET' || req.method === 'OPTIONS') {
+        console.log(`Method: ${req.method} || URL: ${req.url}`);
+        next();
+        return;
+    }
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader?.split(' ')[1];
+
+    if(!token)res.status(401).send('No token provided');
+
+    try{
+        const payload = await verifyToken(token as string);
+        if(payload){
+            next();
+        }else{
+            res.status(403).send('Invalid or expired token');
+        }
+    }catch(err){
+        res.status(401).send(err);
+    }
+}
+
+export default userRouter;
+
