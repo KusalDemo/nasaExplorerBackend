a59ae374aef8f016fa2083cb761ea910a14bb939
KusalDemo <kusalgunasekara2002@gmail.com>
2025-02-27 07:51:39 +0530

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3001
+
+
+app.listen(port,()=>{
+    console.log(`Server is up and running on port : ${port}`)
+})
